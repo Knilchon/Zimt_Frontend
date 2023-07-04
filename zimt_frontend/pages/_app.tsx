e3d2e1a077177ba7209944ed9d76a5f62316cb48
HomerDEV1 <andreyheinz@protonmail.com>
2023-07-04 09:19:38 +0200

    Rough UI Design - Functions to be added

--- zimt_frontend/pages/_app.tsx
+++ zimt_frontend/pages/_app.tsx
@@ -1,5 +1,20 @@
 import type { AppProps } from 'next/app'
+import '../styles/_app.css'
 
 export default function App() {
-  return <div>Kartoffelsalat</div>
+  return (
+    <>
+      <header className='App-header'>
+        <p>Das ist ein Header.</p>
+      </header>
+      <div className='map-and-sidebar'>
+        <div className='side-bar'>
+          <p>Das ist eine Sidebar.</p>
+        </div>
+        <div className='map-body'>
+          <p>Das ist die große Oberfläche neben der Sidebar.</p>
+        </div>
+      </div>
+    </>
+  )
 }
