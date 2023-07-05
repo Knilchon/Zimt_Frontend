256bc47bba81df1f869b4f9abc56c733c53258f4
wulandari <fucdce@gmail.com>
2023-07-05 22:31:35 +0200

    cleaned up index.tsx to pull

--- zimt_frontend/pages/index.tsx
+++ zimt_frontend/pages/index.tsx
@@ -1,15 +1,16 @@
 import React from 'react';
 import MapSvg from '../components/MapSvg';
 
-const handlePathClick = (id: string) => {
-  console.log('Clicked path ID:', id);
-};
+//const handlePathClick = (id: string) => {
+  //console.log('Clicked path ID:', id);
+//};
 
 const App = () => (
   <>
-    <h1>SVG page</h1>
-    <MapSvg handlePathClick={handlePathClick} />
+  
   </>
 );
+ 
+
 
 export default App;
