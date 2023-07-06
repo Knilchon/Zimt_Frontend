ecbbce2f3113339c4982e2a50086e8a43ed4830f
HomerDEV1 <andreyheinz@protonmail.com>
2023-07-06 09:56:31 +0200

    LookupPage ready to use

--- zimt_frontend/pages/_app.tsx
+++ zimt_frontend/pages/_app.tsx
@@ -71,7 +71,7 @@ export default function App() {
         />
         <div className='map-body'>
           Put map here.
-          <LevelSelector selectedLevel={selectedLevel} setSelectedLevel={setSelectedLevel} />
+          {/*<LevelSelector selectedLevel={selectedLevel} setSelectedLevel={setSelectedLevel} />*/}
           {selectedRoom ? (<LookupPage selectedRoom={selectedRoom} className='lookup-page'/>):(<></>) }
           
         </div>
